f81e6eda97d40ff8c442992bbd16a1764f6d4244
seiya1011 <seiya.f10111@icloud.com>
2021-05-30 14:13:50 +0900

    fist commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,61 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit,:show,:destroy,:update]
+  before_action :move_to_index, only: [:edit, :update]
+  def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+    redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @prototype.destroy
+      redirect_to root_path
+    else
+      render :root_path
+    end
+  end
+
+  
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+def set_prototype
+  @prototype = Prototype.find(params[:id])
+end
+
+def move_to_index
+  redirect_to root_path unless current_user == @prototype.user
+
+  end
+end
